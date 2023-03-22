0225cda2346c7f790fed064973fea96dfe406db7
edimamatthew <edimamatthew@users.noreply.github.com>
2023-03-22 19:24:22 +0000

    Fix styling

--- app/Http/Controllers/BlockedSlotController.php
+++ app/Http/Controllers/BlockedSlotController.php
@@ -11,24 +11,28 @@ class BlockedSlotController extends Controller
     public function index()
     {
         $blockedSlots = BlockedSlot::get();
+
         return view('blocked-slot.index', compact('blockedSlots'));
     }
     
     public function create()
     {
         $slots = Slot::get();
+
         return view('blocked-slot.form', compact('slots'));
     }
     
     public function store(CreateBlockedSlotRequest $request)
     {
         BlockedSlot::create($request->all());
+
         return redirect()->route('blocked-slots.index');
     }
 
     public function destroy(BlockedSlot $blockedSlot)
     {
         $blockedSlot->delete();
+
         return back();
     }
 }
